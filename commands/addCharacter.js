e9e5015da026c43f11f90b99ecac34f18757d4f1
theprinceraj <discordsimpcity+github@gmail.com>
2023-07-26 19:19:59 +0530

    .

--- commands/addCharacter.js
+++ commands/addCharacter.js
@@ -1,22 +1,3 @@
-const config = require('../config.json');
-const { Deta } = require('deta');
-const deta = Deta(config.detaSpaceKey);
-const db = deta.Base('cards');
-const cardSchema = {
-    version: 1,
-    characterName: String,
-    seriesName: String,
-    wishlistCount: Number,
-    imageUrl: String,
-    cardsIncirculation: Number,
-};
-
-async function addCard(card) {
-    const result = await db.put(card);
-    console.log('Card added:', result.key);
-}
-
-
 exports.run = (client, message, args) => {
 
 }
